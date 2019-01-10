2002cdcb4dd7911e0b001ba50276bc0db80080c7
rofehr <roland@diefehrs.de>
2019-01-11 00:02:58 +0100

    Update osdgst.c

--- osdgst.c
+++ osdgst.c
@@ -74,7 +74,7 @@ void *cOsdgst::CreateWindow(Display *dpy)
             if(!pict_format)
                 continue;
 
-            if( (pict_format->direct.alphaMask > 0) && (pict_format->depth == 32) ) {
+             if( (pict_format->direct.alphaMask > 0) && (pict_format->depth == 32) ) {
                 fbconfig = fbconfigs[i];
                 break;
             }
