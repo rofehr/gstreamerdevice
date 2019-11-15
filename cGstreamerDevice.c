c1d6f67a8b8eb7ae00b846c5821e1a62d0cb3bcb
rofehr <roland@diefehrs.de>
2019-11-15 20:14:37 +0100

    Test 'autovideosink'

--- cGstreamerDevice.c
+++ cGstreamerDevice.c
@@ -119,7 +119,7 @@ void cGstreamerDevice::Action(void) {
 
 
     
-    pipeline = gst_parse_launch("appsrc name=vdrsource !  decodebin  name=demux  demux.  !  queue  !  audioconvert  !  audioresample  !  autoaudiosink demux. !  videoconvert  !  xvimagesink " , NULL);
+    pipeline = gst_parse_launch("appsrc name=vdrsource !  decodebin  name=demux  demux.  !  queue  !  audioconvert  !  audioresample  !  autoaudiosink demux. !  videoconvert  !  autovideosink" , NULL);
 	//pipeline = gst_parse_launch("appsrc name=vdrsource !  decodebin  name=demux  demux.  !  queue  !  audioconvert  !  audioresample  !  autoaudiosink demux. !  videoconvert  !  glimagesink" , NULL);
     if (!pipeline) {
      isyslog("!pipeline");
